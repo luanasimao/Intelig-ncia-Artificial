d99506bc3ca5bded0369086f045de51908c4153b
luanasimao <162707293+luanasimao@users.noreply.github.com>
2024-06-14 09:26:15 -0300

    .

--- script.js
+++ script.js
@@ -49,7 +49,17 @@ let perguntaAtual;
 function mostraPergunta() {
     perguntaAtual = perguntas[atual];
     caixaPerguntas.textContent = perguntaAtual.enunciado;
+    mostraAlternativas();
 }
+
+function mostraAlternativas() {
+    for (const alternativa of perguntaAtual.alternativas) {
+      const botaoAlternativas = document.createElement("button");
+      botaoAlternativa.textContent = alternativa;
+      caixaAlternativas.appendChild(botaoAlternativas);
+    }
+  }
+  
 
 mostraPergunta()
 
